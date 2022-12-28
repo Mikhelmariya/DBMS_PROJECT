7be2125499e48a73ea21bc9d6feb8290869351d8
ENV <ENV@DESKTOP-VVFS1CL>
2022-12-28 21:30:14 +0530

    DBMS PROJECT

---
+++ src/dbms_project/DBMS_Project.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dbms_project;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JTable;
+import java.sql.Statement;
+
+
+
+
+/**
+ *
+ * @author ENV
+ */
+public class DBMS_Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
